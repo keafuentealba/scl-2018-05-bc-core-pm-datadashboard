95cecc299a2a01d9f0b8c37a872ba06a752e64a7
camila fuentealba <camila.fuentealba.rubilar@gmail.com>
2018-06-07 18:08:11 -0400

    funcion para obtener valores country

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
 execute = () => {
-  let country = document.getElementById("enterContry").value;
-}
+  let country = document.getElementById("enterContry").value;//aun no estoy muy segura de esto
+  let buttonContry = document.getElementById("buttonContry").value;//este boton es para obtener el valor del campo de los paises
+};
