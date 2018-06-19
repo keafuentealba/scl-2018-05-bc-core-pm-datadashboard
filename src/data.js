2b7f78ad7c2b3c49d6a282093a77c423082bc557
camila fuentealba <camila.fuentealba.rubilar@gmail.com>
2018-06-19 12:09:29 -0400

    funcion

--- src/data.js
+++ src/data.js
@@ -37,5 +37,52 @@ const getUserStats = (cohortId) => {
 }
 
 window.computeUsersStats = (users, progress, courses) => {
-  console.log(users, progress, courses)
+  console.log(users, progress, courses);
+  for(let i = 0; i < users.length; i++){
+    const user_progress = progress[users[i].id];
+    let total_percent = 0;
+    let courses = Object.keys(user_progress).map(key => user_progress[key]);
+    console.log(courses);
+    let parts = [];
+    courses.forEach((course) => {
+      const parts_aux = Object.keys(course.units).map((key) => {
+        return Object.keys(course.units[key].parts).map((key_parts) => {
+          return course.units[key].parts[key_parts]
+        });
+      });
+      parts = parts.concat.apply([], parts_aux);
+    });
+    const exercises = parts.filter((part) => {return part.type === 'practice'});
+    const reads = parts.filter((part) => {return part.type === 'read'});
+    const quizzes = parts.filter((part) => {return part.type === 'quiz'});
+    users[i].stats = {
+      exercises: {
+        total: exercises.length,
+        completed: exercises.filter(excercise => excercise.completed === 1).length
+      },
+      reads: {
+        total: reads.length,
+        completed: reads.filter(read => read.completed === 1).length,
+      },
+      quizzes: {
+        total: quizzes.length,
+        completed: quizzes.filter(quiz => quiz.completed === 1).length,
+        scoreSum: quizzes.reduce((previous, current) => {
+          let score = 0;
+          if(current.score !== undefined){
+            score = current.score;
+          }
+          return previous + score}, 0),
+      }
+    };
+    users[i].stats.exercises.percent = parseInt(users[i].stats.exercises.completed / users[i].stats.exercises.total * 100);
+    users[i].stats.reads.percent = parseInt(users[i].stats.reads.completed / users[i].stats.reads.total * 100);
+    users[i].stats.quizzes.percent = parseInt(users[i].stats.quizzes.completed / users[i].stats.quizzes.total * 100);
+    users[i].stats.quizzes.scoreAvg = parseInt(users[i].stats.quizzes.scoreSum / users[i].stats.quizzes.total)
+    users[i].stats.percent = courses.reduce((previous, current) => {
+      return previous + current.percent;
+    }, 0) / courses.length
+  }
+  console.log(progress["00hJv4mzvqM3D9kBy3dfxoJyFV82"]);
+  console.log(users);
 }
